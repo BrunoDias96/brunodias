90be8cf0f6f1b76e36933996467d12877a691f60
BrunoDias96 <50370653+BrunoDias96@users.noreply.github.com>
2019-05-21 12:17:52 +0100

    Add files via upload

---
+++ brunodias.py
@@ -0,0 +1,4 @@
+def chavier():
+	print("Chavier é com CH.")
+def pars():
+	print("Oh Pars")
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='brunodias',
+      version='0.1',
+      description='Chavier Distribution Package',
+      author='Bruno Dias',
+      author_email='Your Email',
+      url='The URL project - github for example',
+      py_modules=['brunodias'],
+     )
